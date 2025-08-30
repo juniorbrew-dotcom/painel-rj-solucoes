a989cd2663866fcfeb35f216d8be75831414fa94
juniorbrew-dotcom <juniorbrew@gmail.com>
2025-08-30 11:33:52 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,20 @@
-const CACHE_NAME = 'painel-rj-solucoes-v1';
-const URLS_TO_CACHE = [
-  '/painel-rj-solucoes/',
-  '/painel-rj-solucoes/index.html',
-  '/painel-rj-solucoes/manifest.json',
-  '/painel-rj-solucoes/icon-192.png',
-  '/painel-rj-solucoes/icon-512.png'
-];
-
-// Instala o Service Worker e armazena arquivos
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+    caches.open("rj-cache").then(cache => {
+      return cache.addAll([
+        "index.html",
+        "manifest.json",
+        "icon-192.png",
+        "icon-512.png"
+      ]);
+    })
   );
 });
 
-// Ativa e limpa caches antigos
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-});
-
-// Intercepta requisições e serve do cache
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
